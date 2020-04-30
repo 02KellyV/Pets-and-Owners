5d6943fa8f8e79025000d4f0ff641227949eaf5b
02KellyV <900@holbertonschool.com>
2020-04-30 20:12:54 +0000

    add Pet

--- api/v1/views/pets.py
+++ api/v1/views/pets.py
@@ -1,28 +1,94 @@
 #!/usr/bin/python3
-""" API """
-from flask import Flask, jsonify, abort, request
-from models import storage
-import json
+"""Contains pets view"""
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, abort, make_response, request
+from models import storage, pets, owners
 
-
-@app_views.route('/pets',  methods=['GET'],
+@app_views.route('/pets', 
+                 methods=['GET'],
                  strict_slashes=False)
 def pets():
-    """
-    All Pets
-    """
+    """pets"""
     pets = []
-    all_pets = storage.all('Pets')
-    return jsonify(all_pets.to_dict)
+    my_pets = storage.all('Pet').values()
+    for my_pet in my_pets:
+        pets.append(my_pet.to_dict())
+    return jsonify(pets)
 
 
-@app_views.route('/pets/<pet_id>',  methods=['GET'],
+@app_views.route('/owners/<string:owner_id>/pets',
+                 methods=['GET'],
+                 strict_slashes=False)
+def pets_owner_id(owner_id):
+    """Retrieves a Owner object"""
+    pets = []
+    my_owner = storage.get('Owner', owner_id)
+    if my_owner is None:
+        abort(404)
+    for my_pet in my_owner.pets:
+        pets.append(my_pet.to_dict())
+    return jsonify(pets)
+
+
+@app_views.route('/pets/<string:pet_id>',
+                 methods=['GET'],
                  strict_slashes=False)
 def pet_id(pet_id):
-    """
-    Pet
-    """
-    pet = storage.get('Pet', pet_id)
-    if not pet:
+    """Retrieves a Pet object by id"""
+    my_pet = storage.get('Pet', pet_id)
+    if my_pet is None:
         abort(404)
-    return jsonify(pet.to_dict())
+    return jsonify(my_pet.to_dict())
+
+
+@app_views.route('/pets/<string:pet_id>',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def pet_id_delete(pet_id):
+    """Deletes a Pet object by id"""
+    my_pet = storage.get('Pet', pet_id)
+    if my_pet is None:
+        abort(404)
+    my_pet.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('owners/<owner_id>/pets',
+                 methods=['POST'],
+                 strict_slashes=False)
+def create_pet(pet_id):
+    """Creates a Pet object"""
+    my_owner = storage.get('Owner', owner_id)
+    if my_owner is None:
+        abort(404)
+    if not request.json:
+        abort(400, 'Not a JSON')
+    if 'name' not in request.json:
+        abort(400, 'Missing name')
+    my_pet = pet.Pet(name=request.json.get('name', ""), owner_id=owner_id)
+    storage.new(my_pet)
+    my_pet.save()
+    return make_response(jsonify(my_pet.to_dict()), 201)
+
+
+@app_views.route('/pets/<string:pet_id>',
+                 methods=['PUT'],
+                 strict_slashes=False)
+def update_pet(pet_id):
+    """Updates a Pet object"""
+    my_pet = storage.get('Pet', pet_id)
+    if my_pet is None:
+        abort(404)
+    if not request.json:
+        # return make_response(jsonify({'error': 'Not a JSON'}), 400)
+        abort(400, 'Not a JSON')
+    for req in request.json:
+        if req not in ['id', 'created_at', 'updated_at']:
+            setattr(my_pet, req, request.json[req])
+    my_pet.save()
+    return jsonify(my_pet.to_dict())
+
+
+if __name__ == "__main__":
+    pass
