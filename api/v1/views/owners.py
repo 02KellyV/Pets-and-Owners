ec0a1b529259aafe5cdc521a9c4d490dac93f629
danielcinome <danielchinome987@gmail.com>
2020-04-30 20:11:59 +0000

    Owners

--- api/v1/views/owners.py
+++ api/v1/views/owners.py
@@ -2,15 +2,70 @@
 """ API """
 from flask import Flask, jsonify, abort, request
 from models import storage
+from models.pet import Pet
+from models.owner import Owner
+from api.v1.views import app_views
 import json
 
 
-@app_views.route('/owners',  methods=['GET'],
+@app_views.route('/owners', methods=['GET', 'POST'],
                  strict_slashes=False)
 def owners():
     """
     All owners
     """
     owners = []
-    all_owners = storage.all('Owner')
-    return jsonify(all_owners.to_dict())
+    if request.method == 'GET':
+        all_owners = storage.all('Owner')
+        return jsonify(all_owners.to_dict())
+    elif request.method == 'POST':
+        if not request.get_json():
+            return jsonify('Not a Json'), 400
+        data = request.get_json()
+        if not('firstname' in data):
+            return jsonify('Not firstname'), 400
+        new_owner = Owner(**data)
+        storage.new(new_owner)
+        storage.save()
+        return jsonify(new_owner), 201
+
+
+@app_views.route('/owners/<owner_id>', methods=['PUT', 'DELETE'],
+                 strict_slashes=False)
+def owner_id(owner_id):
+    """
+    Owner
+    """
+    owner = storage.get('Owner', owner_id)
+    if not owner:
+        abort(404)
+    if request.method == 'PUT':
+        if not request.get_json:
+            return jsonify('Not a Json'), 400
+        data = request.get_json()
+        for k, v in data.items():
+            setattr(owner, k, v)
+        storage.save()
+        return jsonify(owner.to_dict()), 201
+    elif request.method == 'DELETE':
+        storage.delete(owner)
+        storage.save()
+        return ({}), 200
+
+
+@app_views.route('/owners/<owner_id>/pets',  methods=['GET'],
+                 strict_slashes=False)
+def owner_pet(owner_id):
+    """
+    All pets for a owner
+    """
+    list_pet = []
+    owner = storage.get('Owner', owner_id)
+    if not owner:
+        abort(404)
+    pets = storage.all('Pet')
+    if obj in pets.values():
+        obj = obj.to_dict()
+        if obj['owner'] == owner_id:
+            list_pet.append(obj)
+    return jsonify(list_pet)
