95baa852aec6409ccab890ca62ba63de2666e339
danielcinome <danielchinome987@gmail.com>
2020-04-30 19:40:05 +0000

    Get pets

--- api/v1/views/pets.py
+++ api/v1/views/pets.py
@@ -1,56 +1,28 @@
 #!/usr/bin/python3
 """ API """
 from flask import Flask, jsonify, abort, request
-from api.v1.views import app_views
 from models import storage
-from models.state import State
-from models.city import City
-from models.user import User
 import json
 
 
-@app_views.route('/users',  methods=['GET', 'POST'],
+@app_views.route('/pets',  methods=['GET'],
                  strict_slashes=False)
-def user():
-    """Return list of users"""
-    users = []
-    if request.method == 'GET':
-        all_users = storage.all('User')
-        for user in all_users.values():
-            users.append(user.to_dict())
-        return jsonify(users)
-    elif request.method == 'POST':
-        if not request.get_json():
-            return jsonify('Not a JSON'), 400
-        data = request.get_json()
-        if not('email' in data):
-            return jsonify('Missing email'), 400
-        if not('password' in data):
-            return jsonify('Missing password'), 400
-        new_user = User(**data)
-        storage.new(new_user)
-        storage.save()
-        return jsonify(new_user.to_dict()), 201
+def pets():
+    """
+    All Pets
+    """
+    pets = []
+    all_pets = storage.all('Pets')
+    return jsonify(all_pets.to_dict)
 
 
-@app_views.route('/users/<user_id>', methods=['GET', 'DELETE', 'PUT'],
+@app_views.route('/pets/<pet_id>',  methods=['GET'],
                  strict_slashes=False)
-def user_id(user_id):
-    """User id"""
-    user = storage.get('User', user_id)
-    if not user:
+def pet_id(pet_id):
+    """
+    Pet
+    """
+    pet = storage.get('Pet', pet_id)
+    if not pet:
         abort(404)
-    if request.method == 'GET':
-        return jsonify(user.to_dict())
-    elif request.method == 'DELETE':
-        storage.delete(user)
-        storage.save()
-        return jsonify({}), 200
-    elif request.method == 'PUT':
-        if not request.get_json():
-            return jsonify('Not a JSON'), 400
-        data = request.get_json()
-        for k, v in data.items():
-            setattr(user, k, v)
-        storage.save()
-        return jsonify(user.to_dict()), 200
+    return jsonify(pet.to_dict())
